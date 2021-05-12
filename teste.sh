ac10676d30ec75415c6c0cbb896fd86d969e90b7
Gabriel Monteiro <gabriel.bsilva@bandtec.com.br>
2021-05-12 20:04:46 -0300

    teste.sh

---
+++ teste.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Removendo travas eventuais do apt ##
+
+sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock ;
+
+## Atualizando o repositório ##
+
+sudo apt update
+
+#Instalando a nossa aplicação
+
