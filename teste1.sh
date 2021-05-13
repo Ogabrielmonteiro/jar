57c23716a7009d515b6f527db559bbb9088e212f
Gabriel Monteiro <gabriel.bsilva@bandtec.com.br>
2021-05-13 14:12:30 -0300

    teste 7

--- teste1.sh
+++ teste1.sh
@@ -19,13 +19,11 @@ cd /home/$USER/Desktop/Quality-System
 #Clonando repositorio do GIT
 git clone https://github.com/Ogabrielmonteiro/jar.git
 
-#Entrando na pasta do clone
-cd /home/$USER/Desktop/Quality-System/jar/3.0
-
 #Instalando o jdk 11
 sudo apt install default-jre -y
 
-#Entrando na pasta do jar
+#Entrando na pasta do clone
+cd /home/$USER/Desktop/Quality-System/jar/jarProjeto
 
 #Executando o jar
 java -jar teste-api-1.0-SNAPSHOT-jar-with-dependencies.jar
