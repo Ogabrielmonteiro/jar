1217321f996a45e7e06e22179466a44358f8c59f
carlosgomes1 <carlos_gomes.s@hotmail.com>
2021-05-24 19:27:40 -0300

    adicionando cores e manivela

--- script/script1.sh
+++ script/script1.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "Olá "$USER", bem-vindo ao script de instalação da Quality System."
-echo "Estamos verificando sua versão do java..."
+PURPLE='0;35'
+NC='\033[0m' 
+
+echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Olá "$USER", bem-vindo ao script de instalação da Quality System."
+echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Estamos verificando sua versão do java..."
 
 abrir_software() {
-    echo "Você deseja abrir o software agora? (sim/nao)"
+    echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Você deseja abrir o software agora? (sim/nao)"
     read abrir
 
     if [ \"$abrir\" == \"sim\" ]; 
@@ -11,16 +14,16 @@ abrir_software() {
         cd /home/$USER/Desktop/Quality-System/jar/jarProjeto
 
         sleep 1
-        echo "Abrindo nossa aplicação"
+        echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Abrindo nossa aplicação"
         java -jar teste-api-1.0-SNAPSHOT-jar-with-dependencies.jar
     else
-        echo "blz maluco"
+        echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) OK, até a próxima!"
         exit 0
     fi
 }
 
 instalar_aplicacao() {
-    echo "Deseja realizar a instalação do software da Quality System? (sim/nao)"
+    echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Deseja realizar a instalação do software da Quality System? (sim/nao)"
         read repositorio
 
         if [ \"$repositorio\" == \"sim\" ]; 
@@ -34,39 +37,39 @@ instalar_aplicacao() {
 
             abrir_software
         else
-            echo "Você optou por não instalar o software, até a próxima."
+            echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Você optou por não instalar o software, até a próxima."
             exit 0
         fi
 }
 
 instalar_java() {
-    echo "Não foi identificado nenhuma versão do Java =("
-    echo "Você deseja realizar a instalação do Java? (sim/nao)"
+    echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Não foi identificado nenhuma versão do Java =("
+    echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Você deseja realizar a instalação do Java? (sim/nao)"
     read confirm
 
     if [ \"$confirm\" == \"sim\" ];
     then
 
-        echo "Vamos realizar os updates dos pacotes..."
+        echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Vamos realizar os updates dos pacotes..."
         sleep 1
         sudo add-apt-repository ppa:webupd8team/java -y
 
-        echo "Instalando... quase lá!"
+        echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Instalando... quase lá!"
 
         sudo apt update -y
 
-        echo "Instalando Java..."
+        echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Instalando Java..."
         sleep 1
-        sudo apt install default-jre -y 1> /dev/null 2> /dev/stdout
+        sudo apt install default-jre -y
 
-        echo "Java instalado com sucesso"
+        echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Java instalado com sucesso"
 
         sleep 1
 
         instalar_aplicacao
 
         else
-            echo "Okay, você optou por não instalar o Java no momento, até a próxima."
+            echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Okay, você optou por não instalar o Java no momento, até a próxima."
             exit 0
     fi
 }
@@ -76,7 +79,7 @@ verificar_java() {
         then
             instalar_java
         else
-            echo "Legal! Você já possui o Java instalado."
+            echo "$(tput setaf 10)[Manivela]:$(tput setaf 7) Legal! Você já possui o Java instalado."
             instalar_aplicacao
     fi
 }
