1c00707cf2dddbdb85a42258895289221df2369b
carlosgomes1 <carlos_gomes.s@hotmail.com>
2021-05-24 19:04:05 -0300

    adicionando funcoes

--- script/script1.sh
+++ script/script1.sh
@@ -2,63 +2,25 @@
 echo "Olá "$USER", bem-vindo ao script de instalação da Quality System."
 echo "Estamos verificando sua versão do java..."
 
-sleep 2
-java --version
-if [ $? -eq 0 ]; 
-then
-    echo "Legal! Você já possui o Java instalado."
-    echo "Deseja instalar nossa aplicação? (s/nao)"
-    read macaco
+abrir_software() {
+    echo "Você deseja abrir o software agora? (s/nao)"
+    read abrir
 
-    if [ \"$macaco\" == \"s\" ]; 
-        then
-            mkdir /home/$USER/Desktop/Quality-System
-            cd /home/$USER/Desktop/Quality-System
-
-            git clone https://github.com/Ogabrielmonteiro/jar.git
-
-            sleep 2
-            clear
-
-            echo "Você deseja abrir o software agora? (s/nao)"
-            read abrir
-
-            if [ \"abrir\" == \"s\" ]; then
-                cd /home/$USER/Desktop/Quality-System/jar/jarProjeto 
-
-                
-
-                java -jar teste-api-1.0-SNAPSHOT-jar-with-dependencies.jar
-            fi
-        else
-            echo "Você optou por não instalar o software, até a próxima."
-        fi
-else
-    echo "Não foi identificado nenhuma versão do Java =("
-    echo "Você deseja realizar a instalão do Java? (s/nao)"
-    read install
-
-    if [ \"$install\" == \"s\" ]; 
+    if [ \"abrir\" == \"s\" ]; 
     then
-        echo "Vamos realizar os updates dos pacotes..."
-        sleep 2
-        sudo add-apt-repository ppa:webupd8team/java -y
+        cd /home/$USER/Desktop/Quality-System/jar/jarProjeto
 
-        echo "Instalando... quase lá!"
+        sleep 1
+        echo "Abrindo nossa aplicação"
+        java -jar teste-api-1.0-SNAPSHOT-jar-with-dependencies.jar
+    else
+        echo "blz maluco"
+        exit 0
+    fi
+}
 
-        sudo apt update -y
-
-        echo "Instalando Java..."
-        sleep 2
-        sudo apt install default-jre -y
-
-        clear
-
-        echo "Java instalado com sucesso"
-
-        sleep 2
-
-        echo "Deseja realizar a instalação do software da Quality System? (s/nao)"
+instalar_aplicacao() {
+    echo "Deseja realizar a instalação do software da Quality System? (s/nao)"
         read repositorio
 
         if [ \"$repositorio\" == \"s\" ]; 
@@ -68,24 +30,52 @@ else
 
             git clone https://github.com/Ogabrielmonteiro/jar.git
 
-            sleep 2
-            clear
+            sleep 1
 
-            echo "Você deseja abrir o software agora? (s/nao)"
-            read abrir
-
-            if [ \"abrir\" == \"s\" ]; 
-            then
-                cd /home/$USER/Desktop/Quality-System/jar/jarProjeto
-
-                sleep 2
-                echo "Abrindo nossa aplicação"
-                java -jar teste-api-1.0-SNAPSHOT-jar-with-dependencies.jar
-            fi
+            abrir_software
         else
             echo "Você optou por não instalar o software, até a próxima."
+            exit 0
         fi
-    else
-        echo "Okay, você optou por não instalar o Java no momento, até a próxima."
-    fi
-fi
+}
+
+instalar_java() {
+    echo "Não foi identificado nenhuma versão do Java =("
+    echo "Você deseja realizar a instalão do Java? (s/nao)"
+    read java
+
+    if [ \"$java\" == \"s\" ]; then
+        echo "Vamos realizar os updates dos pacotes..."
+        sleep 1
+        sudo add-apt-repository ppa:webupd8team/java -y
+
+        echo "Instalando... quase lá!"
+
+        sudo apt update -y
+
+        echo "Instalando Java..."
+        sleep 1
+        sudo apt install default-jre -y 1> /dev/null 2> /dev/stdout
+
+        echo "Java instalado com sucesso"
+
+        sleep 1
+
+        instalar_aplicacao
+
+        else
+            echo "Okay, você optou por não instalar o Java no momento, até a próxima."
+            exit 0
+
+}
+
+verificar_java() {
+    if [ "$(dpkg --get-selections | grep 'default-jre' | wc -l)" -eq "0" ]; 
+        then
+            instalar_java
+        else
+            echo "Legal! Você já possui o Java instalado."
+            instalar_aplicacao
+}
+
+verificar_java
